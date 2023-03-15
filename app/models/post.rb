408efcadf1a8f58b8800b4eb85b73942761e8a5b
JohnOkari <johnnokari@gmail.com>
2023-03-15 13:16:35 +0300

    Done with lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
-end
+    validate :clickbait_title
+  
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
+    private
+  
+    def clickbait_title
+      unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") && title.match(/\d+/) || title.include?("Guess"))
+        errors.add(:title, "must be sufficiently clickbait-y")
+      end
+    end
+  end
+  
